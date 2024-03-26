dca828b1cb07020ace258b2a94e99d34c090876f
cdh <cdh@mail.weathervietnam.vn>
2024-03-26 10:02:46 +0700

    deploy kttv

---
+++ start-container-new-kttv2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# The default node number is 3
+N=6
+
+
+# Start hadoop slave container
+i=3
+while [ $i -lt $N ]
+do
+	sudo docker rm -f hadoop-slave$i &> /dev/null
+	echo "start hadoop-slave$i container..."
+	sudo docker run -itd \
+	                --net=hadoop \
+					-v /opt/bigdata/datanode$i:/root/hdfs/datanode	\
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+	                ngovanmanh/hadoop:3.2.0 &> /dev/null
+	i=$(( $i + 1 ))
+done 
+
+# Get into hadoop master container
+sudo docker exec -it hadoop-master bash
---
+++ start-container-new.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# The default node number is 3
+N=${1:-4}
+
+
+# Start hadoop master container
+sudo docker rm -f hadoop-master &> /dev/null
+
+echo "start hadoop-master container..."
+#sudo docker run -itd --net=hadoop  -p 50070:50070 -p 60010:60010  -p 8088:8088 	-p 9870:9870 -v /home/cdh/VienAI/data/hadoop/namenode:root/hdfs/namenode --name hadoop-master --hostname hadoop-master   ngovanmanh/hadoop:3.2.0
+sudo docker run -itd \
+                --net=hadoop \
+                -p 50070:50070 \
+				-p 60010:60010 \
+                -p 8088:8088 \
+				-p 9870:9870	\
+				-v /home/cdh/VienAI/data/hadoop/namenode:/root/hdfs/namenode	\
+                --name hadoop-master \
+                --hostname hadoop-master \
+                ngovanmanh/hadoop:3.2.0 &> /dev/null
+
+
+# Start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+	sudo docker rm -f hadoop-slave$i &> /dev/null
+	echo "start hadoop-slave$i container..."
+	sudo docker run -itd \
+	                --net=hadoop \
+					-v /home/cdh/VienAI/data/hadoop/datanode$i:/root/hdfs/datanode	\
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+	                ngovanmanh/hadoop:3.2.0 &> /dev/null
+	i=$(( $i + 1 ))
+done 
+
+# Get into hadoop master container
+sudo docker exec -it hadoop-master bash
