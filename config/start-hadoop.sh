4a656cd01eac00b95a9b9f0c67087076051118dd
cdh <cdh@mail.weathervietnam.vn>
2024-03-26 14:44:14 +0700

    deploy

--- config/start-hadoop.sh
+++ config/start-hadoop.sh
@@ -4,6 +4,7 @@ echo -e "\n"
 
 /usr/local/hadoop/bin/hdfs namenode -format
 
+
 echo -e "\n"
 
 $HADOOP_HOME/sbin/start-dfs.sh
@@ -14,3 +15,4 @@ $HADOOP_HOME/sbin/start-yarn.sh
 
 echo -e "\n"
 
+bash /usr/local/hbase/bin/start-hbase.sh
